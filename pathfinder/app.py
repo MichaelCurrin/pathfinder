9b6f2b0258595a2d7cccbf3c91e28867020e7611
Michael Currin <michael.currin@gmail.com>
2018-01-09 22:15:02 +0200

    chore: Create app.py skeleton

---
+++ pathfinder/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Main application file.
+
+Check whether inputs URIs are valid or not and either send an email with the
+result or print the result.
+"""
+
+
+def send_mail():
+    pass
+
+
+def read_csv():
+    pass
+
+
+def validate_uri(uri):
+    pass
+
+
+def process_uri():
+    pass
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
