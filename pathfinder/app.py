5c00969baadc9bcfb47432f10fd98d5df3855695
Michael Currin <michael.currin@gmail.com>
2018-01-09 22:31:17 +0200

    feature: Update app.py

--- pathfinder/app.py
+++ pathfinder/app.py
@@ -7,23 +7,63 @@ result or print the result.
 """
 
 
-def send_mail():
+def send_mail(from_addr, to_addr, passwd, subject, msg):
+    """Send an email using Gmail using provided details and content.
+
+    @param from_addr:
+    @param to_addr:
+    @param passwd:
+    @param subject:
+    @param msg:
+
+    @return: None
+    """
     pass
 
 
-def read_csv():
+def read_csv(fpath):
+    """Read path to CSV and return data.
+
+    @param fpath: path to input CSV file, as absolute path or relative to
+        the app dir.
+
+    @return: two-dimensional list representing rows and columns of CSV data.
+    """
     pass
 
 
 def validate_uri(uri):
+    """Check whether a URI is valid.
+
+    @param uri: URI to validate as string.
+
+    @return: True if URI request is successful and False otherwise.
+    """
     pass
 
 
-def process_uri():
+def process_uris(uris, condition, email_credentials):
+    """Iterate through URIs and handle mailing of the validation results.
+
+    @param uris:
+    @param condition:
+    @param email_credentials:
+
+    @return: None
+    """
     pass
 
 
-def run():
+def run(subject, titles, uris, email_trigger=None):
+    """Valid given URIs with convenient titles and mail the results.
+
+    @param subject:
+    @parma titles:
+    @param uris:
+    @param email_trigger:
+
+    @param: None
+    """
     pass
 
 
