6df3eb813714e1665268908d2b50ac5ebf5bc21a
Michael Currin <michael.currin@gmail.com>
2018-01-09 23:47:57 +0200

    feature: Update config.py

---
+++ pathfinder/lib/config.py
@@ -0,0 +1,27 @@
+"""Application configuration file."""
+import os
+from ConfigParser import SafeConfigParser
+
+
+class AppConf(SafeConfigParser):
+    """App configuration object.
+
+    Make app configuration filenames absolute paths and relative to app
+    config dir. Then configure the conf object with data.
+
+    The local app conf file is optional and in values in it will overwrite
+    those set in the main app conf file
+    """
+
+    def __init__(self):
+        """
+        Initialise instance of AppConf class.
+        """
+        SafeConfigParser.__init__(self)
+
+        self.appDir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                   os.path.pardir))
+        confNames = ('app.conf', 'app.local.conf')
+        confPaths = [os.path.join(self.appDir, 'etc', c) for c in confNames]
+
+        self.read(confPaths)
