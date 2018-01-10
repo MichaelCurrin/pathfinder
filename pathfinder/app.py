cc10e4f9d3230ac64103fa756e000edda510a080
Michael Currin <michael.currin@gmail.com>
2018-01-10 23:28:53 +0200

    feature: Update app.py with more functionality

--- pathfinder/app.py
+++ pathfinder/app.py
@@ -3,9 +3,16 @@
 Main application file.
 
 Check whether inputs URIs are valid or not and either send an email with the
-result or print the result.
+result. Also prints all results.
 """
+import argparse
+
+import requests
+
 from lib import read_csv, send_mail
+from lib.config import AppConf
+
+conf = AppConf()
 
 
 def validate_uri(uri):
@@ -13,35 +20,53 @@ def validate_uri(uri):
 
     @param uri: URI to validate as string.
 
-    @return: True if URI request is successful and False otherwise.
+    @return: True if URI request is results in 200 response code, otherwise
+        return False for any other response code or request errors.
     """
-    pass
+    try:
+        response = requests.get(uri, timeout=conf.getfloat('Scrape', 'timeout'))
+    except requests.exceptions.RequestException:
+        return False
+
+    if response.status_code == 200:
+        return True
+    else:
+        return False
 
 
-def process_uris(uris, condition, email_credentials):
-    """Iterate through URIs and handle mailing of the validation results.
+def validate_uris(uri_data):
+    """Validate the input URIs and return results.
 
-    @param uris:
-    @param condition:
-    @param email_credentials:
-
-    @return: None
     """
-    pass
+    for data in uri_data:
+        data['result'] = validate_uri(data['URI'])
 
 
-def run(subject, titles, uris, email_trigger=None):
-    """Valid given URIs with convenient titles and mail the results.
+def run(uri_data, subject):
+    validate_uris(uri_data)
 
-    @param subject:
-    @parma titles:
-    @param uris:
-    @param email_trigger:
+    template = "{result:10} {title:20} {uri}"
 
-    @param: None
-    """
-    pass
+    formatted = [
+        template.format(
+            result='OK' if data['result'] else 'INVALID',
+            title=data['title'],
+            uri=data['URI']
+        )
+        for data in uri_data
+    ]
 
+    # Print all the results as a report, regardless of the conditions.
+    for f in formatted:
+        print(f)
 
-if __name__ == '__main__':
-    run()
+    # Process single email for all URIs and only include URIs outcomes which
+    # meet their condition. No mail might be sent.
+    outbound = []
+    for i, data in enumerate(uri_data):
+        if (data['notify'] == 'always' or
+            (data['result'] is True and data['notify'] == 'valid') or
+                (data['result'] is False and data['notify'] == 'invalid')):
+            outbound.append(formatted[i])
+
+    send_mail(subject, "\n".join(outbound))
