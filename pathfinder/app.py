fb8acff5df90a1c178181949e80d2800d177a06f
Michael Currin <michael.currin@gmail.com>
2018-01-14 10:58:55 +0200

    refactor: Change result from boolean to string values feature: Add argument parser

--- pathfinder/app.py
+++ pathfinder/app.py
@@ -20,20 +20,20 @@ def validate_uri(uri):
 
     @param uri: URI to validate as string.
 
-    @return: True if URI request is results in 200 response code, otherwise
-        return False for any other response code or request errors.
+    @return: 'OK' if URI request is results in 200 response code, otherwise
+        return 'Invalid' for any other response code or request errors.
     """
     print("Validating: {0}".format(uri))
 
     try:
         response = requests.get(uri, timeout=conf.getfloat('Scrape', 'timeout'))
     except requests.exceptions.RequestException:
-        return False
+        return 'Invalid'
 
     if response.status_code == 200:
-        return True
+        return 'OK'
     else:
-        return False
+        return 'Invalid'
 
 
 def build_html(title, uri_data):
@@ -76,7 +76,7 @@ def build_html(title, uri_data):
             <tr>
                 <td>{result}</td>
                 <td>{title}</td>
-                <td>{URI}</td>
+                <td> <a href="{URI}">{URI}</a> </td>
             </tr>
     """
 
@@ -88,7 +88,7 @@ def build_html(title, uri_data):
     )
 
 
-def run(uri_data, subject):
+def run(uri_data, subject=None):
     """Validate the given URIs then print and email the results.
 
     @param uri_data: List of rows of data which each row is a dictionary
@@ -98,7 +98,8 @@ def run(uri_data, subject):
                 'URI': str,
                 'notify': str
             }
-    @param subject: Message to use for the subject of the mail.
+    @param subject: Message to use for the subject of the mail. If not set,
+        no mail will be sent.
 
     @return: None
     """
@@ -111,7 +112,7 @@ def run(uri_data, subject):
 
     rows_as_text = [
         template.format(
-            result='OK' if row['result'] else 'Invalid',
+            result=row['result'],
             title=row['title'],
             uri=row['URI']
         ) for row in uri_data
@@ -122,25 +123,132 @@ def run(uri_data, subject):
     print(plain_text)
     print()
 
-    # Handle mail an HTML report, for rows matching notification conditions.
-    matched_rows = []
-    for data in uri_data:
-        assert data['notify'] in ('always', 'valid', 'invalid'), \
-            "Got notify value as '{0}' but expected value as one of:"\
-            " always, valid, invalid".format(data['notify'])
+    if subject:
+        # Handle mail an HTML report, for rows matching notification conditions.
+        matched_rows = []
+        for data in uri_data:
+            assert data['notify'] in ('always', 'valid', 'invalid'), \
+                "Got notify value as '{0}' but expected value as one of:"\
+                " always, valid, invalid".format(data['notify'])
 
-        if (data['notify'] == 'always' or
-            (data['result'] is True and data['notify'] == 'valid') or
-                (data['result'] is False and data['notify'] == 'invalid')):
-            matched_rows.append(data)
+            if (data['notify'] == 'always' or
+                (data['result'] == 'OK' and data['notify'] == 'valid') or
+                (data['result'] == 'Invalid' and
+                    data['notify'] == 'invalid')):
+                matched_rows.append(data)
 
-    if matched_rows:
-        html = build_html(subject, matched_rows)
-        send_mail(
-            subject,
-            plain_text=plain_text,
-            html=html
-        )
-        print("Send mail with {0} rows.".format(len(matched_rows)))
+        if matched_rows:
+            count = len(matched_rows)
+            print("Sending mail with {count} matched row{plural}.".format(
+                count=count,
+                plural="s" if count != 1 else ""
+            ))
+            html = build_html(subject, matched_rows)
+            send_mail(
+                subject,
+                plain_text=plain_text,
+                html=html
+            )
+            print("Sent mail.")
+        else:
+            print("Zero rows matched, so no mail was sent.")
+
+
+def main():
+    """Main function to handle command-line arguments.
+
+    The logic for setting the current command using dest of subparser
+    was borrowed from here:
+        https://stackoverflow.com/questions/8250010/argparse-identify-which-subparser-was-used
+
+    @return: None
+    """
+    parser = argparse.ArgumentParser(
+        description="Pathfinder app. Expects input values set on command-line"
+            " or in a CSV file, then prints output and sends an email"
+            " where notification conditions are met."
+    )
+
+    subparsers = parser.add_subparsers(
+        help="sub-command help",
+        dest='command'
+    )
+
+    path_parser = subparsers.add_parser(
+        'file',
+        help="Specify persistent file containing rows of URI data to validate",
+    )
+    path_parser.add_argument(
+        'file',
+        metavar='PATH',
+        help="Path to CSV file. See var/lib/paths.csv.template as example"
+            " of header format. All rows are validated, the results are "
+            " printed and a mail is sent if at least on row result meets"
+            " its notify condition."
+    )
+    path_parser.add_argument(
+        '-n', '--no-send',
+        action='store_true',
+        help="If supplied, override the default behavior and prevent a"
+            " mail from being sent."
+    )
+    path_parser.add_argument(
+        '--subject',
+        help="Subject to use in the mail. Defaults to value set on config file"
+            " if not set. Must be quoted to include spaces."
+    )
+
+    custom_parser = subparsers.add_parser(
+        'custom',
+        help="Specify a single URI to validate using command-line arguments",
+    )
+    custom_parser.add_argument(
+        'title',
+        metavar='TITLE',
+        help="Short label describing the URI. Must be quoted to include spaces."
+    )
+    custom_parser.add_argument(
+        'uri',
+        metavar='URI',
+        help="URI to validate."
+    )
+    custom_parser.add_argument(
+        '-n', '--no-send',
+        action='store_true',
+        help="If supplied, override the default behavior and prevent a"
+            " mail from being sent."
+    )
+    custom_parser.add_argument(
+        '--subject',
+        help="Subject to use in the mail. Defaults to value set on config file"
+            " if not set. Must be quoted to include spaces."
+    )
+
+    args = parser.parse_args()
+
+    if not args.command:
+        args.print_help()
     else:
-        print("Zero rows matched so no mail was sent.")
+        if args.command == 'file':
+            uri_data = read_csv(args.file)
+        else:
+            row = {
+                'title': args.title,
+                'URI': args.uri,
+                'notify': 'always'
+            }
+            uri_data = [row]
+
+        if args.no_send:
+            subject = None
+        else:
+            if args.subject:
+                subject = args.subject
+            else:
+                subject = conf.get('Email', 'default_subject')
+
+        run(uri_data, subject)
+
+
+if __name__ == '__main__':
+    main()
