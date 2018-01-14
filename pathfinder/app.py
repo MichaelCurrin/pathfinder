41967df52679ee6c5d10a4598dd3f71c052f4c29
Michael Currin <michael.currin@gmail.com>
2018-01-14 14:42:15 +0200

    feature: Add data validation and improved HTML styling in app.py

--- pathfinder/app.py
+++ pathfinder/app.py
@@ -6,11 +6,16 @@ Check whether inputs URIs are valid or not and either send an email with the
 result. Also prints all results.
 """
 import argparse
+import datetime
+import sys
 
 import requests
+from formencode import Invalid
 
 from lib import read_csv, send_mail
 from lib.config import AppConf
+from lib.validators import UriData
+
 
 conf = AppConf()
 
@@ -36,29 +41,48 @@ def validate_uri(uri):
         return 'Invalid'
 
 
-def build_html(title, uri_data):
+def build_html(mail_title, uri_data):
     """Use templates to build an HTML mail with dynamic table rows.
 
-    @param title: Title value to use in metadata and body.
+    Footer is kept at the bottom of the page thanks to:
+        http://matthewjamestaylor.com/blog/keeping-footers-at-the-bottom-of-the-page
+
+    @param page_title: Title value to use in metadata and body.
     @param uri_data: List of URI rows to add to the HTML table output.
 
-    @return: Completed HTML template with rows of URI data.
+    @return: HTML template with title and table rows filled in.
     """
     base = """
 <!DOCTYPE html>
 <html>
     <head>
-        <title>{title}</title>
+        <title>{mail_title}</title>
+
         <style>
+
+            html {{
+                font-family: Arial, Helvetica, sans-serif;
+            }}
+
             table, th, td {{
                 border: 1px solid black;
                 border-collapse: collapse;
             }}
+
+            footer {{
+               position:absolute;
+               width:100%;
+               bottom:0;
+               height:60px;
+               padding-left: 5px;
+               background:#ddd;
+            }}
+
         </style>
     </head>
 
     <body>
-        <h1>{title}</h1>
+        <h2>{mail_title}</h2>
 
         <table>
             <tr>
@@ -83,7 +107,7 @@ def build_html(title, uri_data):
     formatted_rows = [row_template.format(**row) for row in uri_data]
 
     return base.format(
-        title=title,
+        mail_title=mail_title,
         table_data="\n".join(formatted_rows)
     )
 
@@ -98,39 +122,53 @@ def run(uri_data, subject=None):
                 'URI': str,
                 'notify': str
             }
-    @param subject: Message to use for the subject of the mail. If not set,
-        no mail will be sent.
+    @param subject: Message to use for the subject and heading of the
+        mail. The current date will be added to the end of the subject.
+        Leave the subject set to None to prevent sending of a mail.
 
     @return: None
     """
+    # Clean the data first.
+    uri_schema = UriData()
+    cleaned_data = []
     for row in uri_data:
+        try:
+            cleaned_row = uri_schema.to_python(row)
+        except Invalid as e:
+            # Print a brief error message and show the problem row, which
+            # should not be prettified in case that raises an error. Then exit.
+            print("{error}. {message} \n".format(
+                error=type(e).__name__,
+                message=str(e)
+            ))
+            print("Input row: \n {0}".format(row))
+            sys.exit(1)
+
+        cleaned_data.append(cleaned_row)
+
+    # Perform the main validation logic which this app is based around.
+    for row in cleaned_data:
         row['result'] = validate_uri(row['URI'])
 
     # Handle printing of plain text report of all rows.
     template = "{result:10} {title:20} {uri}"
     header = template.format(result="Result", title="Title", uri="URI")
-
-    rows_as_text = [
+    plain_text_rows = [
         template.format(
             result=row['result'],
             title=row['title'],
             uri=row['URI']
-        ) for row in uri_data
+        ) for row in cleaned_data
     ]
-    plain_text = "\n".join(
-        [header, "=" * len(header)] + rows_as_text
-    )
-    print(plain_text)
-    print()
+    print(header)
+    print("=" * len(header))
+    for row in plain_text_rows:
+        print(row)
 
     if subject:
         # Handle mail an HTML report, for rows matching notification conditions.
         matched_rows = []
-        for data in uri_data:
-            assert data['notify'] in ('always', 'valid', 'invalid'), \
-                "Got notify value as '{0}' but expected value as one of:"\
-                " always, valid, invalid".format(data['notify'])
-
+        for data in cleaned_data:
             if (data['notify'] == 'always' or
                 (data['result'] == 'OK' and data['notify'] == 'valid') or
                 (data['result'] == 'Invalid' and
@@ -143,10 +181,27 @@ def run(uri_data, subject=None):
                 count=count,
                 plural="s" if count != 1 else ""
             ))
-            html = build_html(subject, matched_rows)
+
+            subject_with_date = "{subject} {date}".format(
+                subject=subject,
+                date=str(datetime.date.today())
+            )
+            matched_rows_as_text = [
+                template.format(
+                    result=row['result'],
+                    title=row['title'],
+                    uri=row['URI']
+                ) for row in cleaned_data
+            ]
+            plain_text = [header, "=" * len(header)] + matched_rows_as_text
+            html = build_html(
+                mail_title=subject_with_date,
+                uri_data=matched_rows
+            )
+
             send_mail(
-                subject,
-                plain_text=plain_text,
+                subject=subject_with_date,
+                plain_text="\n".join(plain_text),
                 html=html
             )
             print("Sent mail.")
@@ -166,7 +221,7 @@ def main():
     parser = argparse.ArgumentParser(
         description="Pathfinder app. Expects input values set on command-line"
             " or in a CSV file, then prints output and sends an email"
-            " where notification conditions are met."
+            " for notification conditions which are met."
     )
 
     subparsers = parser.add_subparsers(
@@ -181,10 +236,12 @@ def main():
     path_parser.add_argument(
         'file',
         metavar='PATH',
-        help="Path to CSV file. See var/lib/paths.csv.template as example"
-            " of header format. All rows are validated, the results are "
-            " printed and a mail is sent if at least on row result meets"
-            " its notify condition."
+        help="Path to CSV file. See var/lib/paths.csv.template for"
+            " required header format. A 'notify' value must be one of:"
+            " 'valid', 'invalid' or 'always'. URIs in all rows will be"
+            " validated, the results will be printed and a mail of just the"
+            " rows meeting the notify condition will be sent. Or no mail, if"
+            " no conditions are met."
     )
     path_parser.add_argument(
         '-n', '--no-send',
