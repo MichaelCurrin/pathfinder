a11719ef819e3248da5336f6390719f7d4e2b2d5
MichaelCurrin <michael.currin@gmail.com>
2018-01-17 18:51:46 +0200

    feature: Allow additional config file to be read

--- pathfinder/lib/config.py
+++ pathfinder/lib/config.py
@@ -10,18 +10,31 @@ class AppConf(SafeConfigParser):
     config dir. Then configure the conf object with data.
 
     The local app conf file is optional and in values in it will overwrite
-    those set in the main app conf file
+    those set in the main app conf file.
     """
 
     def __init__(self):
         """
         Initialise instance of AppConf class.
+
+        Read config files in three locations, expecting the first versioned
+        file to always be present and the two optional files to either override
+        the default values or be ignored silently if they are missing.
         """
         SafeConfigParser.__init__(self)
 
+        # Path to the top-level pathfinder directory in the repo.
         self.appDir = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                                    os.path.pardir))
-        confNames = ('app.conf', 'app.local.conf')
-        confPaths = [os.path.join(self.appDir, 'etc', c) for c in confNames]
+
+        etcConfNames = ('app.conf', 'app.local.conf')
+        confPaths = [os.path.join(self.appDir, 'etc', c) for c in etcConfNames]
+
+        userConfigPath = os.path.join(
+            os.path.expanduser('~')
+            '.config',
+            'pathfinder.conf'
+        )
+        confPaths.append(userConfigPath)
 
         self.read(confPaths)
